0ab07bbc1633d68d06dded76b005a4dea51f9166
Vikas Tuteja <tuteja1987@gmail.com>
2020-12-26 15:09:16 +0530

    basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { ContentComponent } from './components/content/content.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LatestnewsComponent } from './components/latestnews/latestnews.component';
+import { LibraryComponent } from './components/library/library.component';
+import { OurmissionComponent } from './components/ourmission/ourmission.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'latest-news', component: LatestnewsComponent },
+  { path: 'audio-library', component: LibraryComponent },
+  { path: 'content', component: ContentComponent },
+  { path: 'our-mission', component: OurmissionComponent },
+  { path: 'about-us', component: AboutusComponent },
+  { path: 'contact-us', component: ContactusComponent },
+  { path: 'faqs', component: FaqsComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { OurmissionComponent } from './components/ourmission/ourmission.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { LatestnewsComponent } from './components/latestnews/latestnews.component';
+import { LibraryComponent } from './components/library/library.component';
+import { ContentComponent } from './components/content/content.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutusComponent,
+    ContactusComponent,
+    OurmissionComponent,
+    FaqsComponent,
+    LatestnewsComponent,
+    LibraryComponent,
+    ContentComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
