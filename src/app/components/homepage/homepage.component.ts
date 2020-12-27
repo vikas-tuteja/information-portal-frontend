de66faefe1a10d0e18b885ad24bbde1effbfa147
Vikas Tuteja <tuteja1987@gmail.com>
2020-12-27 20:55:34 +0530

    home page css

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -6,11 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  images!: any;
   constructor() { }
-
+  images = ["", "/assets/images/sikh-farmer.jpeg", "/assets/images/farmer2.jpeg", "https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg"];
+  cards = [
+    {
+      title: 'Card Title 1',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Read More',
+      img: this.images[0]    },
+    {
+      title: 'Card Title 2',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Read More',
+      img: this.images[1]
+    },
+    {
+      title: 'Card Title 3',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Read More',
+      img: this.images[2]
+    },
+    {
+      title: 'Card Title 4',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Read More',
+      img: this.images[3]
+    },
+    {
+      title: 'Card Title 5',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Read More',
+      img: this.images[1]
+    }
+  ];
+  slides: any = [[]];
+  chunk(arr: any, chunkSize: number) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
   ngOnInit(): void {
-    this.images = ["", "/assets/images/sikh-farmer.jpeg", "/assets/images/farmer2.jpeg", "https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg"];
+    this.slides = this.chunk(this.cards, 3);
   }
 
 }
